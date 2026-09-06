e2f210e92816de486561002709edf3fbc0e11c62
feduardo1977-arch <feduardo1977@gmail.com>
2026-09-06 12:37:35 -0600

    V5
    V5

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Service worker mínimo: necesario para que Android/Chrome consideren
 // esta página como una PWA instalable de verdad (requisito técnico).
-const CACHE_NAME = 'poliglota-v3';
+const CACHE_NAME = 'poliglota-v4';
 const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
